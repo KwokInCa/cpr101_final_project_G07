8137ee551de30ad188c9aba09d5278150a59edd0
Tin Mei Timmy Kwok <tmtkwok@myseneca.ca>
2021-04-20 18:32:04 -0400

    fixed

--- tokenizing.c
+++ tokenizing.c
@@ -6,13 +6,15 @@ void tokenizing() {
 
 	printf("*** Start of Tokenizing Sentences Demo ***\n"); 
 	char sentences[200]; 
+	char* end_token;
+
 	char* sentence; 
 	int s_counter;
 	while (TRUE) {
 		printf("Type a few sentences separated by dot(q - to quit):\n"); 
 		gets(sentences); 
 		if (strcmp(sentences, "q") == 0) break; 
-		sentence = strtok(sentences, "."); 
+		sentence = strtok_r(sentences, ".", &end_token);
 		s_counter = 1; 
 		while (sentence) {
 
@@ -20,7 +22,7 @@ void tokenizing() {
 
 			tokenizing_phrases(sentence);
 
-			sentence = strtok_r(NULL, ".");
+			sentence = strtok_r(NULL, ".", &end_token);
 		}
 	} printf("*** End of Tokenizing Sentences Demo ***\n\n");
 
@@ -29,10 +31,11 @@ void tokenizing() {
 void tokenizing_phrases(char* phrases)
 {
 	printf("*** Start of Tokenizing Phrases Demo ***\n");
+	char* end_token;
 
 	char* phrase;
 	int p_counter;
-	phrase = strtok_r(phrases, ",");
+	phrase = strtok_r(phrases, ",", &end_token);
 
 	p_counter = 1;
 	while (phrase) {
@@ -40,7 +43,7 @@ void tokenizing_phrases(char* phrases)
 
 		tokenizing_words(phrase);
 
-		phrase = strtok_r(NULL, ",");
+		phrase = strtok_r(NULL, ",", &end_token);
 	}
 
 	printf("*** End of Tokenizing Phrases Demo ***\n\n");
@@ -50,13 +53,15 @@ void tokenizing_phrases(char* phrases)
 void tokenizing_words(char* words)
 {
 	printf("*** Start of Tokenizing Words Demo ***\n");
+	char* end_token;
+
 	char* word;
 	int w_counter;
-	word = strtok_r(words, " ");
+	word = strtok_r(words, " ", &end_token);
 	w_counter = 1;
 	while (word) {
 		printf("Word #%d is \'%s\'\n", w_counter++, word);
-		word = strtok_r(NULL, " ");
+		word = strtok_r(NULL, " ", &end_token);
 	}
 	printf("*** End of Tokenizing Words Demo ***\n\n");
 
